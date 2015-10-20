2f322fc3b460790dec01ca02101e77998501e2d8
Sujit Shivaprasad <kgfreak@gmail.com>
2015-10-19 23:16:39 -0400

    parsed excel file for some specific data

--- src/SatellitesOrbitingEarth.py
+++ src/SatellitesOrbitingEarth.py
@@ -7,21 +7,31 @@ The version used here is the most recent'''
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import xlrd
+import xlrd 
+import xlwt
 #import ExtractSatellitesOrbitingEarth
 
-
-'''#Read in the data from the excel file
-wb = xlrd.open_workbook('UCS_Satellite_Database_MostRecent.xlsx')
-#Get first sheet
-first_sheet = wb.sheet_by_index(0)
-print first_sheet.row_values(0)
-print first_sheet.cell(0,0) '''
-
-data = pd.ExcelFile("UCS_Satellite_Database_MostRecent.xlsx")
+#data = pd.ExcelFile("UCS_Satellite_Database_MostRecent.xlsx")
 #print(data.sheet_names)
-df = data.parse("Sheet1")
-print(df.head)
+#df = data.parse("Sheet1")
+#print(df.head)
+
+workbook = xlrd.open_workbook('UCS_Satellite_Database_MostRecent.xls', on_demand = True)
+worksheet = workbook.sheet_by_index(0) #I think UCS usually uses only sheet 1 for their data
+
+max_rows = worksheet.nrows #max rows in sheet
+max_cols = worksheet.ncols #max columns in sheet
+IndiaNum = []; USANum = []
+
+for i in range(0, max_rows):
+	if worksheet.cell(i,1).value == 'India':
+		#print('Found!', i+1)
+		IndiaNum.append(i+1) #All of India's satellite cell values
+	elif worksheet.cell(i,1).value == 'USA':
+		USANum.append(i+1) #All of USA's satellite cell values
+
+NumIndia = len(IndiaNum) #Count India's satellites
+NumUSA = len(USANum) #Count USA's satellites
 
 
 

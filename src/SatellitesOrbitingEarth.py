92889a51d32535876ef9235b9da0767acc24eca2
Sujit Shivaprasad <kgfreak@gmail.com>
2016-02-14 23:17:46 -0500

    starting math model

--- src/SatellitesOrbitingEarth.py
+++ src/SatellitesOrbitingEarth.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import xlrd 
 import xlwt
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
 #import ExtractSatellitesOrbitingEarth
 
 #data = pd.ExcelFile("UCS_Satellite_Database_MostRecent.xlsx")
@@ -23,20 +26,39 @@ max_rows = worksheet.nrows #max rows in sheet
 max_cols = worksheet.ncols #max columns in sheet
 IndiaNum = []; USANum = []
 
+'''
+#Counting the number of satellites by country
 for i in range(0, max_rows):
 	if worksheet.cell(i,1).value == 'India':
 		#print('Found!', i+1)
 		IndiaNum.append(i+1) #All of India's satellite cell values
+		
 	elif worksheet.cell(i,1).value == 'USA':
 		USANum.append(i+1) #All of USA's satellite cell values
 
-NumIndia = len(IndiaNum) #Count India's satellites
-NumUSA = len(USANum) #Count USA's satellites
+#NumIndia = len(IndiaNum) #Count India's satellites
+#NumUSA = len(USANum) #Count USA's satellites
+'''
 
+def g(f,t):
+    #differential equation goes here
 
+def timestep(f,g,h,t):
+    return f + g(f,t)/h
 
+#timestep size
+h = 0.1
+#initial value
+f = [0]
+#initial time
+t = 0
 
+#loop
+while t<=N:
+    append timestep(f,g,h,t) to f
+    t += h
 
+#output f in some way (plot, xls, etc)
 
 
 
